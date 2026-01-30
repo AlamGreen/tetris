014e3735f5905f1d4d31d8e8993e516a8c9a5c6d
AlamGreen <adamkrasnapiorau@gmail.com>
2026-01-30 15:41:01 +0200

    Setup

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,206 @@
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Board board {  get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
+
+    public float stepDelay = 1f;
+    public float lockdelay = 0.5f;
+    private float steptime;
+    private float lockTime;
+
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
+    {
+        this.board = board;
+        this.position = position;
+        this.data = data;
+        this.rotationIndex = 0;
+        this.steptime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
+
+        if (this.cells == null)
+        {
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        this.lockTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            Rotate(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        if (Time.time >= this.steptime)
+        {
+            Step();
+        }
+
+        this.board.Set(this);
+    }
+
+    private void Step()
+    {
+        this.steptime = Time.time + this.stepDelay;
+
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockdelay)
+        {
+            Lock();
+        }
+    }
+
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+
+        Lock();
+    }
+
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.ClearLines();
+        this.board.SpawnPiece();
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+            this.lockTime = 0f;
+        }
+
+        return valid;
+    }
+
+    private void Rotate(int direction)
+    {
+        int orginalRotation = this.rotationIndex;
+        this.rotationIndex += Wrap(this.rotationIndex + direction, 0, 4);
+
+        ApplyRotationMatrix(direction);
+
+        if (!TestWallKickss(this.rotationIndex, direction))
+        {
+            this.rotationIndex = orginalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private bool TestWallKickss(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKiks(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < this.data.wallkicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallkicks[wallKickIndex, i];
+
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKiks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, this.data.wallkicks.GetLength(0));
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input)%(max - min);
+        } else
+        {
+            return min + (input - min) % (max - min);
+        }
+    }
+}
